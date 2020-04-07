8c0ce049efbbcf5bdbe736c03cb61a181d555c58
GD7 <GD7@DESKTOP-UQ6ST6F>
2020-04-07 18:14:26 +0900

    main home 페이지 1차완성

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,7 +1,24 @@
 
 $(document).ready(function(){
 	
-
-
+	//메뉴 아코디언
+	$("#category_btn").click(function(){
+		var $target = $("#menu_accordion");
+		if($target.css("right") == "-400px"){
+			$("#overlay_t").show();
+			$target.animate({right: "0px", opacity:1},800,function(){
+				$("body").css({"overflow":"hidden"});
+				$("#hello").animate({opacity:1},1000);
+			});
+		}
+	});
+	$(".close").click(function(){
+		$("#overlay_t").hide();
+		var $target = $("#menu_accordion");
+		$target.animate({right: "-400px",opacity:0},300,function(){
+			$("body").css("overflow","");
+			$("#hello").css("opacity", 0);
+		});
+	})
 	
 });
